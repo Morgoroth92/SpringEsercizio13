10e865342b5e14b00e2d2f97b80aa6a6dfc8050f
Claudio V. Carta <cvcarta@proton.me>
2024-02-29 10:49:51 +0100

    Initial commit

---
+++ src/main/java/com/example/SpringEsercizio13/SpringEsercizio13Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringEsercizio13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEsercizio13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEsercizio13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringEsercizio13/SpringEsercizio13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringEsercizio13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEsercizio13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
